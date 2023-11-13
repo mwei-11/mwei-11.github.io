9a95c370d830d6668e765d9d91f045a33afa97f4
Myra <myrawei03@gmail.com>
2023-11-13 00:32:46 -0800

    Site updated

--- Script1.js
+++ Script1.js
@@ -1,31 +1,7 @@
-// JavaScript source code
+const experiences = document.querySelectorAll(".experience");
 
-window.addEventListener("scroll", setScrollVar)
-window.addEventListener("resize", setScrollVar)
-
-function setScrollVar() {
-    const htmlElement = document.documentElement
-    const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight
-    htmlElement.style.setProperty("--scroll", Math.min(percentOfScreenHeightScrolled * 100), 100)
-    
-}
-
-setScrollVar()
-
-const observer = new IntersectionObserver(entries => {
-    for (let i = entries.length - 1; i >= 0; i--) {
-        const entry = entries[i]
-        if (entry.isIntersecting) {
-            document.querySelectorAll("[data-img]").forEach(img => {
-                img.classList.remove("show")
-            })
-            const img = document.querySelector(entry.target.dataset.imgToShow)
-            img?.classList.add("show")
-            break
-            }
-    }
-})
-
-document.querySelectorAll("[data-img-to-show]").forEach(section => {
-    observer.observe(section)
-})
+experiences.forEach(experience=> {
+    experience.addEventListener("click", () => {
+        experience.classList.toggle("active");
+    });
+});
